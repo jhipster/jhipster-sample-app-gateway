7605c1da1679564a7eb86b18292936728a04ee5c
Pascal Grimaud <pascalgrimaud@gmail.com>
2016-10-16 14:18:17 +0200

    Init gateway project

---
+++ src/main/java/io/github/jhipster/sample/config/cassandra/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Cassandra specific configuration.
+ */
+package io.github.jhipster.sample.config.cassandra;
---
+++ src/main/webapp/app/blocks/config/localstorage.config.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('jhipsterSampleGatewayApp')
+        .config(localStorageConfig);
+
+    localStorageConfig.$inject = ['$localStorageProvider', '$sessionStorageProvider'];
+
+    function localStorageConfig($localStorageProvider, $sessionStorageProvider) {
+        $localStorageProvider.setKeyPrefix('jhi-');
+        $sessionStorageProvider.setKeyPrefix('jhi-');
+    }
+})();
