746935e1453ee8ac94a39c0ae811a0d02c70c446
Julien Dubois <julien.dubois@gmail.com>
2017-06-23 18:07:12 +0200

    automatic project update

--- src/main/webapp/app/components/alert/alert-error.directive.js
+++ src/main/webapp/app/components/alert/alert-error.directive.js
@@ -51,7 +51,7 @@
                 }).sort();
                 var errorHeader = httpResponse.headers(headers[0]);
                 var entityKey = httpResponse.headers(headers[1]);
-                if (errorHeader) {
+                if (angular.isString(errorHeader)) {
                     var entityName = entityKey;
                     addErrorAlert(errorHeader, errorHeader, {entityName: entityName});
                 } else if (httpResponse.data && httpResponse.data.fieldErrors) {
