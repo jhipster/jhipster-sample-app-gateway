8c5d416612293fd2e4cfff5c211e77fad92d5ccd
Julien Dubois <julien.dubois@gmail.com>
2017-07-06 16:01:41 +0200

    automatic project update

--- src/main/webapp/app/app.constants.js
+++ src/main/webapp/app/app.constants.js
@@ -5,5 +5,6 @@
         .module('jhipsterSampleGatewayApp')
         .constant('VERSION', "0.0.1-SNAPSHOT")
         .constant('DEBUG_INFO_ENABLED', true)
+        .constant('BUILD_TIMESTAMP', "")
 ;
 })();
