e1eb479508145b53bda5b8d74af6a9760be2f7bf
Julien Dubois <julien.dubois@gmail.com>
2017-12-14 10:48:13 +0100

    automatic project update

--- src/main/webapp/app/shared/shared-common.module.ts
+++ src/main/webapp/app/shared/shared-common.module.ts
@@ -1,5 +1,7 @@
 import { NgModule, LOCALE_ID } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { registerLocaleData } from '@angular/common';
+import locale from '@angular/common/locales/en';
 
 import {
     JhipsterSampleGatewaySharedLibsModule,
@@ -28,4 +30,8 @@ import {
         JhiAlertErrorComponent
     ]
 })
-export class JhipsterSampleGatewaySharedCommonModule {}
+export class JhipsterSampleGatewaySharedCommonModule {
+    constructor() {
+        registerLocaleData(locale);
+    }
+}
