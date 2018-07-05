b39dc55bf34c60ac17fe90abaf375f04ebfe84ac
Julien Dubois <julien.dubois@gmail.com>
2018-07-05 12:00:43 +0200

    automatic project update

--- src/main/java/io/github/jhipster/sample/web/rest/GatewayResource.java
+++ src/main/java/io/github/jhipster/sample/web/rest/GatewayResource.java
@@ -9,6 +9,8 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.netflix.zuul.filters.Route;
 import org.springframework.cloud.netflix.zuul.filters.RouteLocator;
 import org.springframework.http.*;
+import org.springframework.security.access.annotation.Secured;
+import io.github.jhipster.sample.security.AuthoritiesConstants;
 import org.springframework.web.bind.annotation.*;
 
 import com.codahale.metrics.annotation.Timed;
@@ -36,6 +38,7 @@ public class GatewayResource {
      */
     @GetMapping("/routes")
     @Timed
+    @Secured(AuthoritiesConstants.ADMIN)
     public ResponseEntity<List<RouteVM>> activeRoutes() {
         List<Route> routes = routeLocator.getRoutes();
         List<RouteVM> routeVMs = new ArrayList<>();
