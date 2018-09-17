ff18bacb25945cd03d11ef09740fc873db6ce552
Julien Dubois <julien.dubois@gmail.com>
2018-09-17 23:30:36 +0200

    automatic project update

--- src/test/javascript/e2e/account/account.spec.ts
+++ src/test/javascript/e2e/account/account.spec.ts
@@ -2,86 +2,91 @@ import { browser, element, by, ExpectedConditions as ec } from 'protractor';
 
 import { NavBarPage, SignInPage, PasswordPage, SettingsPage } from '../page-objects/jhi-page-objects';
 
+const expect = chai.expect;
+
 describe('account', () => {
     let navBarPage: NavBarPage;
     let signInPage: SignInPage;
     let passwordPage: PasswordPage;
     let settingsPage: SettingsPage;
 
-    beforeAll(async () => {
+    before(async () => {
         await browser.get('/');
         navBarPage = new NavBarPage(true);
     });
 
     it('should fail to login with bad password', async () => {
-        const expect1 = /Welcome, Java Hipster!/;
+        const expect1 = 'Welcome, Java Hipster!';
         const value1 = await element(by.css('h1')).getText();
-        expect(value1).toMatch(expect1);
+        expect(value1).to.eq(expect1);
         signInPage = await navBarPage.getSignInPage();
         await signInPage.autoSignInUsing('admin', 'foo');
 
-        const expect2 = /Failed to sign in!/;
+        const expect2 = 'Failed to sign in! Please check your credentials and try again.';
         const value2 = await element(by.css('.alert-danger')).getText();
-        expect(value2).toMatch(expect2);
+        expect(value2).to.eq(expect2);
     });
 
     it('should login successfully with admin account', async () => {
         await browser.get('/');
         signInPage = await navBarPage.getSignInPage();
 
-        const expect1 = /Login/;
+        const expect1 = 'Login';
         const value1 = await element(by.className('username-label')).getText();
-        expect(value1).toMatch(expect1);
+        expect(value1).to.eq(expect1);
         await signInPage.autoSignInUsing('admin', 'admin');
 
-        const expect2 = /You are logged in as user "admin"/;
-        const value2 = element(by.id('home-logged-message'));
-        await browser.wait(ec.visibilityOf(value2), 5000);
-        expect(await value2.getText()).toMatch(expect2);
+        const expect2 = 'You are logged in as user "admin".';
+        await browser.wait(ec.visibilityOf(element(by.id('home-logged-message'))));
+        const value2 = await element(by.id('home-logged-message')).getText();
+        expect(value2).to.eq(expect2);
     });
 
     it('should be able to update settings', async () => {
         settingsPage = await navBarPage.getSettingsPage();
 
-        const expect1 = /User settings for \[admin\]/;
+        const expect1 = 'User settings for [admin]';
         const value1 = await settingsPage.getTitle();
-        expect(value1).toMatch(expect1);
+        expect(value1).to.eq(expect1);
         await settingsPage.save();
 
-        const expect2 = /Settings saved!/;
-        const value2 = await element(by.css('.alert-success')).getText();
-        expect(value2).toMatch(expect2);
+        const expect2 = 'Settings saved!';
+        const alert = element(by.css('.alert-success'));
+        const value2 = await alert.getText();
+        expect(value2).to.eq(expect2);
     });
 
     it('should fail to update password when using incorrect current password', async () => {
         passwordPage = await navBarPage.getPasswordPage();
 
-        expect(await passwordPage.getTitle()).toMatch(/Password for \[admin\]/);
+        expect(await passwordPage.getTitle()).to.eq('Password for [admin]');
 
         await passwordPage.setCurrentPassword('wrong_current_password');
         await passwordPage.setPassword('new_password');
         await passwordPage.setConfirmPassword('new_password');
         await passwordPage.save();
 
-        const expect2 = /An error has occurred! The password could not be changed./;
-        const value2 = await element(by.css('.alert-danger')).getText();
-        expect(value2).toMatch(expect2);
+        const expect2 = 'An error has occurred! The password could not be changed.';
+        const alert = element(by.css('.alert-danger'));
+        const value2 = await alert.getText();
+        expect(value2).to.eq(expect2);
         settingsPage = await navBarPage.getSettingsPage();
     });
 
     it('should be able to update password', async () => {
         passwordPage = await navBarPage.getPasswordPage();
 
-        expect(await passwordPage.getTitle()).toMatch(/Password for \[admin\]/);
+        expect(await passwordPage.getTitle()).to.eq('Password for [admin]');
 
         await passwordPage.setCurrentPassword('admin');
         await passwordPage.setPassword('newpassword');
         await passwordPage.setConfirmPassword('newpassword');
         await passwordPage.save();
 
-        const expect2 = /Password changed!/;
-        const value2 = await element(by.css('.alert-success')).getText();
-        expect(value2).toMatch(expect2);
+        const expect2 = 'Password changed!';
+        const alert = element(by.css('.alert-success'));
+        const value2 = await alert.getText();
+        expect(value2).to.eq(expect2);
         await navBarPage.autoSignOut();
         await navBarPage.goToSignInPage();
         await signInPage.autoSignInUsing('admin', 'newpassword');
@@ -94,7 +99,7 @@ describe('account', () => {
         await passwordPage.save();
     });
 
-    afterAll(async () => {
+    after(async () => {
         await navBarPage.autoSignOut();
     });
 });
--- src/test/javascript/e2e/admin/administration.spec.ts
+++ src/test/javascript/e2e/admin/administration.spec.ts
@@ -2,11 +2,13 @@ import { browser, element, by } from 'protractor';
 
 import { NavBarPage, SignInPage } from '../page-objects/jhi-page-objects';
 
+const expect = chai.expect;
+
 describe('administration', () => {
     let navBarPage: NavBarPage;
     let signInPage: SignInPage;
 
-    beforeAll(async () => {
+    before(async () => {
         await browser.get('/');
         navBarPage = new NavBarPage(true);
         signInPage = await navBarPage.getSignInPage();
@@ -19,47 +21,47 @@ describe('administration', () => {
 
     it('should load user management', async () => {
         await navBarPage.clickOnAdmin('user-management');
-        const expect1 = /Users/;
+        const expect1 = 'Users';
         const value1 = await element(by.id('user-management-page-heading')).getText();
-        expect(value1).toMatch(expect1);
+        expect(value1).to.eq(expect1);
     });
 
     it('should load metrics', async () => {
         await navBarPage.clickOnAdmin('jhi-metrics');
-        const expect1 = /Application Metrics/;
+        const expect1 = 'Application Metrics';
         const value1 = await element(by.id('metrics-page-heading')).getText();
-        expect(value1).toMatch(expect1);
+        expect(value1).to.eq(expect1);
     });
 
     it('should load health', async () => {
         await navBarPage.clickOnAdmin('jhi-health');
-        const expect1 = /Health Checks/;
+        const expect1 = 'Health Checks';
         const value1 = await element(by.id('health-page-heading')).getText();
-        expect(value1).toMatch(expect1);
+        expect(value1).to.eq(expect1);
     });
 
     it('should load configuration', async () => {
         await navBarPage.clickOnAdmin('jhi-configuration');
-        const expect1 = /Configuration/;
+        const expect1 = 'Configuration';
         const value1 = await element(by.id('configuration-page-heading')).getText();
-        expect(value1).toMatch(expect1);
+        expect(value1).to.eq(expect1);
     });
 
     it('should load audits', async () => {
         await navBarPage.clickOnAdmin('audits');
-        const expect1 = /Audits/;
+        const expect1 = 'Audits';
         const value1 = await element(by.id('audits-page-heading')).getText();
-        expect(value1).toMatch(expect1);
+        expect(value1).to.eq(expect1);
     });
 
     it('should load logs', async () => {
         await navBarPage.clickOnAdmin('logs');
-        const expect1 = /Logs/;
+        const expect1 = 'Logs';
         const value1 = await element(by.id('logs-page-heading')).getText();
-        expect(value1).toMatch(expect1);
+        expect(value1).to.eq(expect1);
     });
 
-    afterAll(async () => {
+    after(async () => {
         await navBarPage.autoSignOut();
     });
 });
