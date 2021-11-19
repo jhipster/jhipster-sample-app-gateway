6f9d3d9121e6c7ed13a64cea06299b8474620085
Pascal Grimaud <pascalgrimaud@gmail.com>
2021-11-19 17:34:44 +0100

    automatic project update

--- src/main/webapp/app/entities/jhipsterSampleMicroservice/bank-account/update/bank-account-update.component.spec.ts
+++ src/main/webapp/app/entities/jhipsterSampleMicroservice/bank-account/update/bank-account-update.component.spec.ts
@@ -12,102 +12,100 @@ import { IBankAccount, BankAccount } from '../bank-account.model';
 
 import { BankAccountUpdateComponent } from './bank-account-update.component';
 
-describe('Component Tests', () => {
-  describe('BankAccount Management Update Component', () => {
-    let comp: BankAccountUpdateComponent;
-    let fixture: ComponentFixture<BankAccountUpdateComponent>;
-    let activatedRoute: ActivatedRoute;
-    let bankAccountService: BankAccountService;
+describe('BankAccount Management Update Component', () => {
+  let comp: BankAccountUpdateComponent;
+  let fixture: ComponentFixture<BankAccountUpdateComponent>;
+  let activatedRoute: ActivatedRoute;
+  let bankAccountService: BankAccountService;
 
-    beforeEach(() => {
-      TestBed.configureTestingModule({
-        imports: [HttpClientTestingModule],
-        declarations: [BankAccountUpdateComponent],
-        providers: [FormBuilder, ActivatedRoute],
-      })
-        .overrideTemplate(BankAccountUpdateComponent, '')
-        .compileComponents();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [BankAccountUpdateComponent],
+      providers: [FormBuilder, ActivatedRoute],
+    })
+      .overrideTemplate(BankAccountUpdateComponent, '')
+      .compileComponents();
 
-      fixture = TestBed.createComponent(BankAccountUpdateComponent);
-      activatedRoute = TestBed.inject(ActivatedRoute);
-      bankAccountService = TestBed.inject(BankAccountService);
+    fixture = TestBed.createComponent(BankAccountUpdateComponent);
+    activatedRoute = TestBed.inject(ActivatedRoute);
+    bankAccountService = TestBed.inject(BankAccountService);
 
-      comp = fixture.componentInstance;
+    comp = fixture.componentInstance;
+  });
+
+  describe('ngOnInit', () => {
+    it('Should update editForm', () => {
+      const bankAccount: IBankAccount = { id: 456 };
+
+      activatedRoute.data = of({ bankAccount });
+      comp.ngOnInit();
+
+      expect(comp.editForm.value).toEqual(expect.objectContaining(bankAccount));
+    });
+  });
+
+  describe('save', () => {
+    it('Should call update service on save for existing entity', () => {
+      // GIVEN
+      const saveSubject = new Subject<HttpResponse<BankAccount>>();
+      const bankAccount = { id: 123 };
+      jest.spyOn(bankAccountService, 'update').mockReturnValue(saveSubject);
+      jest.spyOn(comp, 'previousState');
+      activatedRoute.data = of({ bankAccount });
+      comp.ngOnInit();
+
+      // WHEN
+      comp.save();
+      expect(comp.isSaving).toEqual(true);
+      saveSubject.next(new HttpResponse({ body: bankAccount }));
+      saveSubject.complete();
+
+      // THEN
+      expect(comp.previousState).toHaveBeenCalled();
+      expect(bankAccountService.update).toHaveBeenCalledWith(bankAccount);
+      expect(comp.isSaving).toEqual(false);
     });
 
-    describe('ngOnInit', () => {
-      it('Should update editForm', () => {
-        const bankAccount: IBankAccount = { id: 456 };
+    it('Should call create service on save for new entity', () => {
+      // GIVEN
+      const saveSubject = new Subject<HttpResponse<BankAccount>>();
+      const bankAccount = new BankAccount();
+      jest.spyOn(bankAccountService, 'create').mockReturnValue(saveSubject);
+      jest.spyOn(comp, 'previousState');
+      activatedRoute.data = of({ bankAccount });
+      comp.ngOnInit();
 
-        activatedRoute.data = of({ bankAccount });
-        comp.ngOnInit();
+      // WHEN
+      comp.save();
+      expect(comp.isSaving).toEqual(true);
+      saveSubject.next(new HttpResponse({ body: bankAccount }));
+      saveSubject.complete();
 
-        expect(comp.editForm.value).toEqual(expect.objectContaining(bankAccount));
-      });
+      // THEN
+      expect(bankAccountService.create).toHaveBeenCalledWith(bankAccount);
+      expect(comp.isSaving).toEqual(false);
+      expect(comp.previousState).toHaveBeenCalled();
     });
 
-    describe('save', () => {
-      it('Should call update service on save for existing entity', () => {
-        // GIVEN
-        const saveSubject = new Subject<HttpResponse<BankAccount>>();
-        const bankAccount = { id: 123 };
-        jest.spyOn(bankAccountService, 'update').mockReturnValue(saveSubject);
-        jest.spyOn(comp, 'previousState');
-        activatedRoute.data = of({ bankAccount });
-        comp.ngOnInit();
+    it('Should set isSaving to false on error', () => {
+      // GIVEN
+      const saveSubject = new Subject<HttpResponse<BankAccount>>();
+      const bankAccount = { id: 123 };
+      jest.spyOn(bankAccountService, 'update').mockReturnValue(saveSubject);
+      jest.spyOn(comp, 'previousState');
+      activatedRoute.data = of({ bankAccount });
+      comp.ngOnInit();
 
-        // WHEN
-        comp.save();
-        expect(comp.isSaving).toEqual(true);
-        saveSubject.next(new HttpResponse({ body: bankAccount }));
-        saveSubject.complete();
+      // WHEN
+      comp.save();
+      expect(comp.isSaving).toEqual(true);
+      saveSubject.error('This is an error!');
 
-        // THEN
-        expect(comp.previousState).toHaveBeenCalled();
-        expect(bankAccountService.update).toHaveBeenCalledWith(bankAccount);
-        expect(comp.isSaving).toEqual(false);
-      });
-
-      it('Should call create service on save for new entity', () => {
-        // GIVEN
-        const saveSubject = new Subject<HttpResponse<BankAccount>>();
-        const bankAccount = new BankAccount();
-        jest.spyOn(bankAccountService, 'create').mockReturnValue(saveSubject);
-        jest.spyOn(comp, 'previousState');
-        activatedRoute.data = of({ bankAccount });
-        comp.ngOnInit();
-
-        // WHEN
-        comp.save();
-        expect(comp.isSaving).toEqual(true);
-        saveSubject.next(new HttpResponse({ body: bankAccount }));
-        saveSubject.complete();
-
-        // THEN
-        expect(bankAccountService.create).toHaveBeenCalledWith(bankAccount);
-        expect(comp.isSaving).toEqual(false);
-        expect(comp.previousState).toHaveBeenCalled();
-      });
-
-      it('Should set isSaving to false on error', () => {
-        // GIVEN
-        const saveSubject = new Subject<HttpResponse<BankAccount>>();
-        const bankAccount = { id: 123 };
-        jest.spyOn(bankAccountService, 'update').mockReturnValue(saveSubject);
-        jest.spyOn(comp, 'previousState');
-        activatedRoute.data = of({ bankAccount });
-        comp.ngOnInit();
-
-        // WHEN
-        comp.save();
-        expect(comp.isSaving).toEqual(true);
-        saveSubject.error('This is an error!');
-
-        // THEN
-        expect(bankAccountService.update).toHaveBeenCalledWith(bankAccount);
-        expect(comp.isSaving).toEqual(false);
-        expect(comp.previousState).not.toHaveBeenCalled();
-      });
+      // THEN
+      expect(bankAccountService.update).toHaveBeenCalledWith(bankAccount);
+      expect(comp.isSaving).toEqual(false);
+      expect(comp.previousState).not.toHaveBeenCalled();
     });
   });
 });
